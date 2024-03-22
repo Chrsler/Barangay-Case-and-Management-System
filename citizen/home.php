772e76a0c2883910e838bbfbef8416fc3e05dc2a
rachellealco <159418055+rachellealco@users.noreply.github.com>
2024-03-23 00:33:37 +0800

    Add files via upload

---
+++ citizen/home.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--CSS FILE-->
+    <link rel="stylesheet" href="./styles/homepage.css">
+    <!-- BOX ICONS CDN LINK -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>Homepage</title>
+</head>
+
+<body>
+
+    <header>
+
+        <h3 class="brgyname">Barangay Upper Bicutan</h3>
+
+        <nav>
+            <ul class="nav-links">
+                <li>
+                    <a class="active" href="home.php">
+                        <i class='bx bx-home-alt'></i>
+                        <span class="link_name">Home</span>
+                    </a>
+
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-notepad'></i>
+                        <span class="link_name">My Cases</span>
+                    </a>
+                </li>
+
+               <li>
+                    <a href="#">
+                        <i class='bx bx-bell'></i>
+                        <span class="link_name">Notifications</span>
+                        <span class="total-notif">3</span>
+                    </a>
+                </li> 
+
+               
+            </ul>
+
+        </nav>
+
+        <div class="info-logout">
+
+            <h3 class = "acc-name">Rachelle Alcoreza</h3>
+
+            <button class="logout-btn">
+                <span>Logout</span>
+                <span><i class='bx bx-log-out'></i></span>
+            </button>
+        </div>
+    </header>
+
+    <section class="main">
+  
+    <div class="content">
+
+        <div class="logo">
+
+        <img src="./images/brgy-logo.png" alt="">
+        </div>
+
+        <div class="info">
+            <h1>Welcome to <br> Barangay Upper Bicutan <br> Complaint System</h1>
+
+            <ul class="location-contact">
+                <li>
+                <span><i class='bx bx-location-plus' ></i></span>
+                <span>F2WX+JX5, A. Bonifacio St, Taguig, 1630 Metro Manila</span>
+                </li>
+
+                <li>
+                <span><i class='bx bx-tab' ></i></span>
+                <span>0956 369 0898 / 0920 566 5365</span>
+                </li>
+
+                <li>
+                <span><i class='bx bx-phone' ></i></span>
+                <span>02 8839 2296</span>
+                </li>
+
+            </ul>
+
+            <button class="blotter-btn">
+                <span>File a Blotter</span>
+                <span><i class='bx bxs-file-plus' ></i></span>
+            </button>
+
+            <p class ="terms-condition">Read about Terms and Conditions</p>
+
+ <!--CHAT GPT TO-->
+ 
+
+
+    </div>
+
+</section>
+
+
+</body>
+</html>
